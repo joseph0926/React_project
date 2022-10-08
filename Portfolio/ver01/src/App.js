1d3e116c55f2a01114f27ba3acabff275a2d6529
김영훈 <joseph0926@naver.com>
2022-10-08 11:54:47 +0900

    2022.10.08

--- Portfolio/ver01/src/App.js
+++ Portfolio/ver01/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Nav from './components/Navbar/Nav'
+import Sidebar from './components/Sidebar/Sidebar'
+import HomePage from './pages/HomePage'
+import ProjectPage from './pages/ProjectPage'
+import Contact from './pages/Contact'
+import ErrorPage from './pages/ErrorPage'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Nav></Nav>
+      <Sidebar></Sidebar>
+      <Routes>
+        <Route path='/' element={<HomePage></HomePage>}></Route>
+        <Route path='project' element={<ProjectPage></ProjectPage>}></Route>z
+        <Route path='contact' element={<Contact></Contact>}></Route>
+        <Route path='*' element={<ErrorPage></ErrorPage>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ Portfolio/ver01/src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
+import Logo from "./Logo";
+
+const Home = () => {
+  return (
+    <header>
+      <div className={styles.container}>
+        <h5>Hello!</h5>
+        <h1>joseph0926's Portfolio</h1>
+        <h5 className={styles["text-light"]}>BackEnd + FrontEnd</h5>
+        <div className={styles["link-container"]}>
+          <Link to="/contact">Contact</Link>
+          <a href="#about">About</a>
+        </div>
+        <div className={styles.logo}>
+          <Logo></Logo>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Home;
